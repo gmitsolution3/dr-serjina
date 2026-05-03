c49c9cc037058d6102c6b7fcc28486fcaf157ee7
gmitsolution3 <gmitsolution3@gmail.com>
2026-05-03 14:58:12 +0600

    feat: project initialized, shadcn added for styling and added theme

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,17 @@
+import { cn } from "@/lib/utils";
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import {
+  Geist,
+  Geist_Mono,
+  Noto_Sans_Bengali,
+} from "next/font/google";
 import "./globals.css";
+
+const notoBengali = Noto_Sans_Bengali({
+  subsets: ["bengali"],
+  variable: "--font-sans",
+  weight: ["400", "500", "600", "700"],
+});
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -25,7 +36,14 @@ export default function RootLayout({
   return (
     <html
       lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+      className={cn(
+        "h-full",
+        "antialiased",
+        geistSans.variable,
+        geistMono.variable,
+        "font-sans",
+        notoBengali.variable,
+      )}
     >
       <body className="min-h-full flex flex-col">{children}</body>
     </html>
