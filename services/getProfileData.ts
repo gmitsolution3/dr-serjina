26c84252020b39b069acd1936c06430e8e519646
gmitsolution3 <gmitsolution3@gmail.com>
2026-05-06 15:19:31 +0600

    feat: changed backend url variable issue

--- services/getProfileData.ts
+++ services/getProfileData.ts
@@ -1,5 +1,10 @@
+const baseUrl =
+  process.env.NODE_ENV === "development"
+    ? process.env.NEXT_PUBLIC_DEV_API_BASE_URL
+    : process.env.NEXT_PUBLIC_API_BASE_URL;
+
 export async function getProfileData() {
-  const res = await fetch(`${process.env.NEXT_PUBLIC_DEV_API_BASE_URL}/profile`);
+  const res = await fetch(`${baseUrl}/profile`);
 
   return await res.json();
 }
--- services/getServices.ts
+++ services/getServices.ts
@@ -1,6 +1,11 @@
+const baseUrl =
+  process.env.NODE_ENV === "development"
+    ? process.env.NEXT_PUBLIC_DEV_API_BASE_URL
+    : process.env.NEXT_PUBLIC_API_BASE_URL;
+
 export async function getServicesData() {
   const res = await fetch(
-    `${process.env.NEXT_PUBLIC_DEV_API_BASE_URL}/treatment-service?page=1&limit=0`,
+    `${baseUrl}/treatment-service?page=1&limit=0`,
   );
 
   return await res.json();
