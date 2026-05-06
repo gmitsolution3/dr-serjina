b39aaffef3e3c805efe3d5229a356ff2147d1b10
gmitsolution3 <gmitsolution3@gmail.com>
2026-05-06 15:56:10 +0600

    chore: testing deployment

--- services/getProfileData.ts
+++ services/getProfileData.ts
@@ -4,6 +4,7 @@ const baseUrl =
     : process.env.NEXT_PUBLIC_API_BASE_URL;
 
 export async function getProfileData() {
+  console.log(process.env.NODE_ENV)
   const res = await fetch(`${baseUrl}/profile`);
 
   return await res.json();
